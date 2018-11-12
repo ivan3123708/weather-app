ff92930d28f30ef3007d349f5584fcace0416ce6
ivan3123708 <ivan3123708@gmail.com>
2018-11-12 19:29:45 +0100

    Added widget

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,22 @@ import weatherData from './weatherData';
 import '../styles/index.sass';
 
 const days = document.getElementById('days');
+const widgetToggle = document.getElementById('widget-toggle');
+const widget = document.getElementById('widget');
+const widgetDay = document.getElementById('dayName');
+const widgetIcon = document.getElementById('dayIcon');
+const widgetTemp = document.getElementById('dayTemp');
+const widgetWindDir = document.getElementById('dayWindDir');
+const widgetWindSpeed = document.getElementById('dayWindSpeed');
+const widgetType = document.getElementById('dayType');
 
+// CLOSE WIDGET
+widgetToggle.onclick = () => {
+  widget.classList.remove('widget-open');
+  widget.classList.add('widget-closed');
+}
+
+// RENDER ALL DAYS FROM WEATHERDATA TO A LIST
 weatherData.days.map((day) => {
   let dayDiv = document.createElement('div');
   dayDiv.classList.add('day');
@@ -32,6 +47,19 @@ weatherData.days.map((day) => {
   dayDiv.appendChild(dayName);
   dayDiv.appendChild(dayIcon);
   dayDiv.appendChild(dayTemp);
+
+  // SET WIDGET DATA & TOGGLE IT
+  dayDiv.onclick = () => {
+    widgetDay.innerText = day.day;
+    widgetIcon.setAttribute('src', icon);
+    widgetTemp.innerHTML = `Temperature: <b>${day.temp} °C</b>`;
+    widgetWindDir.innerHTML = `Wind Direction: <b>${day.windDirection}</b> <img src="img/${day.windDirection}.png">`;
+    widgetWindSpeed.innerHTML = `Wind Speed: <b>${day.windSpeed} m/s</b>`;
+    widgetType.innerHTML = `Type: <b>${day.type}</b>`;
+
+    widget.classList.remove('widget-closed');
+    widget.classList.add('widget-open');
+  }
 
   days.appendChild(dayDiv);
 });
