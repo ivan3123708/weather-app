76884ab1369e24f049a5c21051e726d0562f38fc
ivan3123708 <ivan3123708@gmail.com>
2018-11-12 17:26:05 +0100

    Implemented days list layout & logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,37 @@
+import weatherData from './weatherData';
 import '../styles/index.sass';
 
-console.log('TEST');
+const days = document.getElementById('days');
+
+weatherData.days.map((day) => {
+  let dayDiv = document.createElement('div');
+  dayDiv.classList.add('day');
+
+  let dayName = document.createElement('p');
+  dayName.classList.add('dayName');
+  dayName.innerText = day.day;
+
+  let dayIcon = document.createElement('img');
+  let icon;
+  switch(day.type) {
+    case 'sunny':
+      icon = 'img/sunny.png';
+      break;
+    case 'rainy':
+      icon = 'img/rainy.jpg';
+      break;
+    case 'cloudy':
+      icon = 'img/cloudy.png';
+  }
+  dayIcon.setAttribute('src', icon);
+
+  let dayTemp = document.createElement('p');
+  dayTemp.classList.add('dayTemp');
+  dayTemp.innerText = `${day.temp} °${weatherData.tempUnit}`;
+
+  dayDiv.appendChild(dayName);
+  dayDiv.appendChild(dayIcon);
+  dayDiv.appendChild(dayTemp);
+
+  days.appendChild(dayDiv);
+});
---
+++ src/js/weatherData.js
@@ -0,0 +1,15 @@
+const weatherData = {
+  tempUnit: 'C',
+  windSpeedUnit: 'm/s',
+  days: [
+    { day: 'Mon', temp: 22, windDirection: 'north-east', windSpeed: 10, type: 'sunny' },
+    { day: 'Tue', temp: 14, windDirection: 'north-west', windSpeed: 14, type: 'rainy' },
+    { day: 'Wed', temp: 17, windDirection: 'south-east', windSpeed: 20, type: 'cloudy' },
+    { day: 'Thu', temp: 19, windDirection: 'south-west', windSpeed: 18, type: 'rainy' },
+    { day: 'Fri', temp: 16, windDirection: 'south-west', windSpeed: 17, type: 'rainy' },
+    { day: 'Sat', temp: 19, windDirection: 'north-east', windSpeed: 11, type: 'cloudy' },
+    { day: 'Sun', temp: 21, windDirection: 'south-east', windSpeed: 10, type: 'sunny' }
+  ]
+};
+
+export default weatherData;
